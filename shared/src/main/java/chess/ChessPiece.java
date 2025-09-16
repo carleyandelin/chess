88fcb6e744cda779fdc1f58c73b83b1fab396c26
carleyandelin <carleyna@byu.edu>
2025-09-16 17:44:19 -0600

    Added code to pass BishopMoveTests.java

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,6 +1,7 @@
 package chess;
 
 import java.util.Collection;
+import java.util.Objects;
 
 /**
  * Represents a single chess piece
@@ -10,7 +11,25 @@ import java.util.Collection;
  */
 public class ChessPiece {
 
+    private final PieceType type;
+    private final ChessGame.TeamColor pieceColor;
     public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
+        this.pieceColor = pieceColor;
+        this.type = type;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessPiece that = (ChessPiece) o;
+        return type == that.type && pieceColor == that.pieceColor;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(type, pieceColor);
     }
 
     /**
@@ -29,14 +48,14 @@ public class ChessPiece {
      * @return Which team this chess piece belongs to
      */
     public ChessGame.TeamColor getTeamColor() {
-        throw new RuntimeException("Not implemented");
+        return pieceColor;
     }
 
     /**
      * @return which type of chess piece this piece is
      */
     public PieceType getPieceType() {
-        throw new RuntimeException("Not implemented");
+        return type;
     }
 
     /**
@@ -47,6 +66,68 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        throw new RuntimeException("Not implemented");
+        var possibleMoves = new java.util.ArrayList<ChessMove>();
+        switch (board.getPiece(myPosition).getPieceType()) {
+            case PieceType.KING -> {
+                // king move logic
+                /*
+                int[] rowMoves = {-1, -1, -1, 0, 0, 1, 1, 1};
+                int[] colMoves = {-1, 0, 1, -1, 1, -1, 0, 1};
+                for (int i = 0; i < 8; i++) {
+                    int newRow = myPosition.getRow() + rowMoves[i];
+                    int newCol = myPosition.getColumn() + colMoves[i];
+                    if (newRow >= 1 && newRow <= 8 && newCol >= 1 && newCol <= 8) {
+                        var newPos = new ChessPosition(newRow, newCol);
+                        var targetPiece = board.getPiece(newPos);
+                        if (targetPiece == null || targetPiece.getTeamColor() != this.getTeamColor()) {
+                            possibleMoves.add(new ChessMove(myPosition, newPos, null));
+                        }
+                    }
+                 }
+                 */
+            }
+            case PieceType.QUEEN -> {
+                // queen move logic
+            }
+            case PieceType.BISHOP -> {
+                // bishop move logic
+                int [] rowDirections = {-1, 1, -1, 1};
+                int [] colDirections = {-1, -1, 1, 1};
+                for (int d = 0; d < rowDirections.length; d++) {
+                    int r = rowDirections[d];
+                    int c = colDirections[d];
+                    int newRow = myPosition.getRow();
+                    int newCol = myPosition.getColumn();
+                    while (true) {
+                        newRow += r;
+                        newCol += c;
+                        if (newRow < 1 || newRow > 8 || newCol < 1 || newCol > 8) break;
+                        var newPos = new ChessPosition(newRow, newCol);
+                        var targetPiece = board.getPiece(newPos);
+                        if (targetPiece == null) {
+                            possibleMoves.add(new ChessMove(myPosition, newPos, null));
+                        } else {
+                            if (targetPiece.getTeamColor() != this.getTeamColor()) {
+                                possibleMoves.add(new ChessMove(myPosition, newPos, null));
+                            }
+                            break; // Stop if blocked
+                        }
+                    }
+                }
+            }
+            case PieceType.KNIGHT -> {
+                // knight move logic
+            }
+            case PieceType.ROOK -> {
+                // rook move logic
+            }
+            case PieceType.PAWN -> {
+                // pawn move logic
+            }
+            default -> {
+                throw new RuntimeException("Unknown piece type");
+            }
+        }
+        return possibleMoves;
     }
 }
