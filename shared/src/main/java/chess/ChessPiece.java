02ee635aaebf2239eee57de4498a1d9c8be44dd2
carleyandelin <carleyna@byu.edu>
2025-09-17 00:15:47 -0600

    Added code for QueenMoveTests

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -86,6 +86,29 @@ public class ChessPiece {
             }
             case PieceType.QUEEN -> {
                 // queen move logic
+                int [] rowDirections = {-1, -1, -1, 0, 0, 1, 1, 1};
+                int [] colDirections = {-1, 0, 1, -1, 1, -1, 0, 1};
+                for (int d = 0; d < rowDirections.length; d++) {
+                    int r = rowDirections[d];
+                    int c = colDirections[d];
+                    int newRow = myPosition.getRow();
+                    int newCol = myPosition.getColumn();
+                    while (true) {
+                        newRow += r;
+                        newCol += c;
+                        if (newRow < 1 || newRow > 8 || newCol < 1 || newCol > 8) break;
+                        var newPos = new ChessPosition(newRow, newCol);
+                        var targetPiece = board.getPiece(newPos);
+                        if (targetPiece == null) {
+                            possibleMoves.add(new ChessMove(myPosition, newPos, null));
+                        } else {
+                            if (targetPiece.getTeamColor() != this.getTeamColor()) {
+                                possibleMoves.add(new ChessMove(myPosition, newPos, null));
+                            }
+                            break; // Stop if blocked
+                        }
+                    }
+                }
             }
             case PieceType.BISHOP -> {
                 // bishop move logic
