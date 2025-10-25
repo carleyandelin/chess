d7c049007424ecfeabbc0fd2ad5987f8fccae1f3
carleyandelin <carleyna@byu.edu>
2025-10-25 03:21:45 -0600

    Added basic user, game, and auth operations in DataAccess.java

--- server/src/main/java/dataaccess/DataAccess.java
+++ server/src/main/java/dataaccess/DataAccess.java
@@ -1,5 +1,24 @@
 package dataaccess;
 
+import model.UserData;
+import model.GameData;
+import model.AuthData;
+
 public interface DataAccess {
     void clear() throws DataAccessException;
+
+    // user operations
+    void insertUser(UserData user) throws DataAccessException;
+    UserData getUser(String username) throws DataAccessException;
+
+    // game operations
+    int insertGame(GameData game) throws DataAccessException;
+    GameData getGame(int gameID) throws DataAccessException;
+    GameData[] listGames() throws DataAccessException;
+    void updateGame(GameData game) throws DataAccessException;
+
+    // auth operations
+    void insertAuth(AuthData auth) throws DataAccessException;
+    AuthData getAuth(String authToken) throws DataAccessException;
+    void deleteAuth(String authToken) throws DataAccessException;
 }
