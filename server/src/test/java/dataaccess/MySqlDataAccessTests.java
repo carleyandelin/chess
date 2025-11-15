88c48ab9c7277e66cf76f8d75311e868b70a6e1c
carleyandelin <carleyna@byu.edu>
2025-11-14 20:09:59 -0700

    added positive and negative tests for each method in MySqlDataAccessTests, not yet implemented though

--- server/src/test/java/dataaccess/MySqlDataAccessTests.java
+++ server/src/test/java/dataaccess/MySqlDataAccessTests.java
@@ -17,4 +17,112 @@ public class MySqlDataAccessTests {
         dataAccess.createTables();
         dataAccess.clear(); // Start with clean database
     }
+
+    // Basic code for testing each method below
+    /*
+    @Test
+    public void testMethod_Positive() {
+        // implement
+    }
+
+    @Test
+    public void testMethod_Negative() {
+        // implement
+    }
+     */
+
+    @Test
+    public void clear() {
+        // implement
+    }
+
+    @Test
+    public void insertUser_Positive() {
+        // implement
+    }
+
+    @Test
+    public void insertUser_Negative() {
+        // implement
+    }
+
+    @Test
+    public void getUser_Positive() {
+        // Tests successful get u
+    }
+
+    @Test
+    public void getUser_Negative() {
+        // implement
+    }
+
+    @Test
+    public void insertGame_Positive() {
+        // implement
+    }
+
+    @Test
+    public void insertGame_Negative() {
+        // implement
+    }
+
+    @Test
+    public void getGame_Positive() {
+        // implement
+    }
+
+    @Test
+    public void getGame_Negative() {
+        // implement
+    }
+
+    @Test
+    public void listGames_Positive() {
+        // implement
+    }
+
+    @Test
+    public void listGames_Negative() {
+        // implement
+    }
+
+    @Test
+    public void updateGame_Positive() {
+        // implement
+    }
+
+    @Test
+    public void updateGame_Negative() {
+        // implement
+    }
+
+    @Test
+    public void insertAuth_Positive() {
+        // implement
+    }
+
+    @Test
+    public void insertAuth_Negative() {
+        // implement
+    }
+
+    @Test
+    public void getAuth_Positive() {
+        // implement
+    }
+
+    @Test
+    public void getAuth_Negative() {
+        // implement
+    }
+
+    @Test
+    public void deleteAuth_Positive() {
+        // implement
+    }
+
+    @Test
+    public void deleteAuth_Negative() {
+        // implement
+    }
 }
