d1c43ab9e55cebb84c160bff3542e1c19b859088
carleyandelin <carleyna@byu.edu>
2025-11-21 03:35:08 -0700

    added server facade class

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,16 +1,20 @@
 import org.junit.jupiter.api.*;
 import server.Server;
 
+// the statice serverfacade line and facade = new serverfacade line are from the github page. don't know
+// why they're causing errors.
 
 public class ServerFacadeTests {
 
     private static Server server;
+    static ServerFacade facade;
 
     @BeforeAll
     public static void init() {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
+        facade = new ServerFacade(port);
     }
 
     @AfterAll
